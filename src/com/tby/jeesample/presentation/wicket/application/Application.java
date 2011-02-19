f16d4e6186afc87a0996dfbc391c8fcc92849dc8
Tobias Bayer <tbayer@noname.(none)>
2011-02-19 09:40:51 +0100

    return home page

--- src/com/tby/jeesample/presentation/wicket/application/Application.java
+++ src/com/tby/jeesample/presentation/wicket/application/Application.java
@@ -1,5 +1,15 @@
 package com.tby.jeesample.presentation.wicket.application;
 
-public class Application {
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.WebApplication;
+
+import com.tby.jeesample.presentation.wicket.pages.Today;
+
+public class Application extends WebApplication {
+
+    @Override
+    public Class<? extends Page> getHomePage() {
+        return Today.class;
+    }
 
 }
