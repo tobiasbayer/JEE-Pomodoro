7bebecfb2facdd7b033f293548cfbe5644d7c1b0
Tobias Bayer <tobayer@googlemail.com>
2011-02-26 14:45:17 +0100

    model objects should be serializable

--- src/com/tby/jeesample/model/IEntity.java
+++ src/com/tby/jeesample/model/IEntity.java
@@ -1,6 +1,8 @@
 package com.tby.jeesample.model;
 
-public interface IEntity {
+import java.io.Serializable;
+
+public interface IEntity extends Serializable {
     public Long getId();
 
     public void setId(Long id);
