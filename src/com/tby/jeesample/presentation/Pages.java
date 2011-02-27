f1108a0dd5356d449c801bc0ff261cc7e871c447
Tobias Bayer <tobayer@googlemail.com>
2011-02-27 12:12:27 +0100

    removed jsf remains

--- src/com/tby/jeesample/presentation/Pages.java
+++
@@ -1,9 +0,0 @@
-package com.tby.jeesample.presentation;
-
-public class Pages {
-    public static final String ACTIVITIES = "activities.xhtml";
-
-    public static final String EDIT_TODO = "editToDo.xhtml";
-
-    public static final String TODAY = "today.xhtml";
-}
